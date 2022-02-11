c36eb623ef932bbed53ff1f06dd79769cd1cd49b
iamabdula <abm8656@gmail.com>
2022-02-11 17:42:53 +0500

    vue basics proj3

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <h1>abdullah bin masood -ASE </h1>
+  <!-- component 1 -->
+
+  <div v-if="showModal">
+    <Modal :title="title" :header="header" :text="text"  theme="dark" @close="toggleModal">
+
+      <p style="color:red">This is a sample paragraph for <b style="color:yellow">SLOTS in Vue</b>. Thanks!!</p>
+      <template v-slot:links >
+        <div >
+          <p>Example of Named Slots</p>
+          <a class="navigation-links" target="_blank" href="https://www.google.com">Google</a> 
+          <a class="navigation-links" target="_blank" href="https://www.youtube.com">Youtube</a> 
+        </div>      
+      </template>
+    </Modal>  
+  </div>
+
+  <!-- SECOND MODAL  -->
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1 style="color:black">This is Second Modal</h1>
+      <p style="color:blue">Hello World!!</p>
+    </Modal>
+
+  </div>
+
+  <div style="margin-top:15%">
+    <button @click="toggleModal">Open First Modal</button>
+    <button @click="toggleModalTwo">Open Second Modal</button>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+      
+export default {
+  name: 'App',
+  components: {Modal},
+  data(){
+    return{
+    title:"Vue App",
+    header:"Signup to header part",
+    text:"A quick brown fox jumps over the lazy dog",
+    showModal:true,
+    showModalTwo:false,
+  }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal=!this.showModal
+
+    },
+    toggleModalTwo(){
+      this.showModalTwo=!this.showModalTwo
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #365e3b;
+  margin-top: 60px;
+}
+.navigation-links{
+  border: 2px solid rgb(131, 212, 123);
+  width: 100px;
+  padding: 3px;
+  margin: 10px 10px;
+  color: rgb(70, 201, 77);
+  text-decoration: none;
+}
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,76 @@
+
+<template>
+    
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="theme ? 'dark' : 'sale' ">
+            <h1>{{title}}</h1>
+            <h4>{{header}}</h4>
+            <h5>{{text}}</h5>
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default ({
+    props:['title','header', 'text', 'theme'],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    },
+  
+    })
+</script>
+
+
+<style scoped>
+h1{
+    color: rgb(8, 235, 117);
+}
+
+.modal{
+    width :400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.modal .actions {
+
+    text-align: center;
+    margin: 40px 0 40px 0;
+    color: rgb(129, 23, 23);
+}
+.modal .actions .a{
+
+    padding: 8px;
+    border:1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin:10px;
+}
+.backdrop{
+    top:0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.sale{
+    border: 2px solid  rgb(218, 17, 10);
+    border-style:dashed;
+    color: rgb(216, 214, 230);
+}
+.dark{
+    background: black;
+    color: rgb(209, 197, 178);
+    
+}
+
+
+</style>
